62b040bef626ffbc903403c17338a30d4e50a418
Tierres Griep <tierresgriep@gmail.com>
2024-02-08 17:54:56 -0300

    PC - 08/02/2024 - 17h53

--- src/app.tsx
+++ src/app.tsx
@@ -15,8 +15,42 @@ export function App() {
 
       <div className='h-px bg-slate-700' />
 
-      <div className='grid grid-cols-3 auto-rows'>
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+        <div className='rounded-md bg-slate-700 p-5 space-y-3'>
+          <span className='text-sm font-medium text-slate-200'>
+            Adicionar nota
+          </span>
+          <p className='text-sm leading-6 text-slate-400'>
+            Grave uma nota em áudio que será convertida para texto automaticamente.
+          </p>
+        </div>
 
+        <div className='rounded-md bg-slate-800 p-5 space-y-3'>
+          <span className='text-sm font-medium text-slate-300'>
+            há 2 dias
+          </span>
+          <p className='text-sm leading-6 text-slate-400'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, iure dicta. Minima vel quo est impedit libero perspiciatis commodi omnis maiores quod accusantium. Nesciunt facilis fugit, sit reiciendis error nemo.
+          </p>
+        </div>
+
+        <div className='rounded-md bg-slate-800 p-5 space-y-3'>
+          <span className='text-sm font-medium text-slate-300'>
+            há 4 dias
+          </span>
+          <p className='text-sm leading-6 text-slate-400'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sapiente voluptatibus ex esse nihil molestias eius tenetur blanditiis possimus quam ut facere, soluta, minima voluptatem recusandae natus adipisci? Veritatis, odit?
+          </p>
+        </div>  
+
+        <div className='rounded-md bg-slate-800 p-5 space-y-3'>
+          <span className='text-sm font-medium text-slate-300'>
+            há 7 dias
+          </span>
+          <p className='text-sm leading-6 text-slate-400'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sapiente voluptatibus ex esse nihil molestias eius tenetur blanditiis possimus quam ut facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nisi odit voluptatem ullam eligendi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque explicabo, vel natus architecto velit odit aut enim vero qui pariatur provident, unde dolorem nisi deserunt nam at molestiae aspernatur similique!
+          </p>
+        </div>              
       </div>
     </div>
     
@@ -96,7 +130,7 @@ export function App() {
 
 
 
-// Dentro da className da 'div' de baixo do form
+// className da 'div' de baixo do form(div que faz aquela linha)
 
 //    'h-px bg-slate-700':
 //  O 'h' representa 'height', e o 'px' representa 1px, então fizemos uma div box com altura de 1 pixel com backgrond color de slate-700, que é um cinza
@@ -108,5 +142,107 @@ export function App() {
 
 // Para criar os blocos de notas criei mais uma div dentro da div principal, logo abaixo da div da linha
 
-// notebook 11:56 
+// 'grid':
+//    Deixa a div em formato de grid
+
+// 'grid-cols-3':
+//    Seleciona a quantidade de colunas por padrão, neste caso 3
+
+// 'auto-rows-[250px]':
+//    Seta o tamanho da altura de todos os elementos do grid para 250px
+//    *Também é possível utilizar o 'height' para cada div interna a div grid, uma de cada vez
+//    *Com os colchetes é possível setar qualquer valor sem ser aqueles valores padrão que multiplicam por 4
+
+
+
+
+//          Primeiro elemento dentro da div GRID
+//          (div de 'adicionar nota')
+
+//  dentro do className:
+
+// 'rounded-md':
+//    Altera o 'border-radius' para um tamanho 'medium', que equivale a 6px
+
+// 'bg-slate-700':
+//    Altera a 'background-color' para um cinza
+
+
+
+
+
+//  Adicionei 'gap-6' no className da div da grid para adicionar um espaçamento de 24px entre as colunas
+
+//  Adicionei os elementos 'span' e 'p' no primeiro quadrado do grid
+
+
+
+
+//    Para formatar a primeira div da grid....
+
+//  Verifiquei no figma que o primeiro bloco tem um espaçamento interno de 20px, então adicionei 'p-5' no className da primeira div do grid(pois 5x4 = 20px)('p' se refere a 'padding')
+
+
+
+
+//    Agora vamos customizar o texto do primeiro bloco
+//  Começando pelo 'span'
+//  Dentro do className...
+
+//  'text-sm':
+//    Altera a fonte para um tamanho 'small' -> uma 'font-size' de 14px
+
+//  'font-medium':
+//    Altera o peso da fonte para 500 -> para 'font-weight: 500;'
+
+//  Alterei a cor do texto para 'slate-200'
+
+
+//  Agora vamos para o 'p'
+//  Dentro do className...
+
+//  'text-sm':
+//    Altera a fonte para um tamanho 'small' -> uma 'font-size' de 14px
+
+//  'leading-6':
+//    Altera o 'line-height' para 6x4 = 24px
+
+//  Alterei a cor do texto para 'slate-400'
+
+
+
+
+//  Voltando para a formatação da primeira div em si...
+
+//  Verifiquei no figma que o espaçamento entre o 'span' e o 'p' é de 12px
+//  Então coloquei no className da div do primeiro bloco o seguinte:
+//    'space-y-3':
+//      Fez com que surgisse um espaçamento vertical de 3x4 = 12px entre todos os elementos 
+
+
+
+
+
+// Copiei e colei 2x a primeira div para gerar mais 2 blocos
+
+// Mudei a cor de fundo dos dois últimos cards para 'slate-800'
+// Alterei o 'span' dos dois últimos cards para "há x dias"
+// Alterei o 'p' dos dois últimos cards para textos aleatórios
+// Alterei a cor do 'span' dos dois últimos cards de slate-200 para 'slate-300'
+
+
+
+// Ou seja, a primeira div dentro do grid tem um padrão de cores diferente dos demais, pois ali que vai ser escrita as notas novas
+// Os demais cards vão ter tons de 'background-color' e do 'span' mais escuros
+
+
+
+// Gerei mais um card e perceba que ele caiu para a linha de baixo por causa do 'grid-cols-3' que faz com que tenhamos no máximo 3 colunas
+
+
+
+// Coloquei um texto grande dentro da última div e o texto saiu pra ford do card
+// Para resolver isso....
+
+// PC 08/02/2024 - 17:53 - aula 1 segue no minuto 48:48
 
