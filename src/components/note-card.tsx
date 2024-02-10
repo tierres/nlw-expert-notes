d58eac566844cabc0c562cc68dea9239c3d31873
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-02-10 18:01:15 -0300

    Notebook - 10/02/2024 - 18h00 - 01:00:00 de vídeo

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
     return (
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+        <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible'>
           <span className='text-sm font-medium text-slate-300'>
             há 2 dias
           </span>
@@ -8,6 +8,30 @@ export function NoteCard() {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, iure dicta. Minima vel quo est impedit libero perspiciatis commodi omnis maiores quod accusantium. Nesciunt facilis fugit, sit reiciendis error nemo.
           </p>
           <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-        </div>
+        </button>
     )
 }
+
+//  Adicionei 'hover:border-2' ao className do card
+//  Ele altera a 'border-width' para 2px
+
+//  O problema é que essa borda ela ocupa espaço do elemento, então ao passar o mouse e ativar a bordar fica um efeito chato, onde o texto fica saindo do lugar
+
+//  Para resolver isso troquei o 'hover-border-2' por 'hover:ring-2' que aplica uma 'box shadow'
+//  Só que essa box shadow funciona da mesma forma que uma borda funciona, só que sem prejudicar o tamanho/posição das letras
+
+//  Para trocar a cor do 'ring-2' para a cor que está no figma adicionei logo em seguida 'hover:ring-slate-600'
+
+
+
+//  Alterei o note-card de 'div' para 'button', para eles ficarem clicáveis e termos estado de foco
+//  Acontece que por padrão os botões tem o texto centralizado, então adicionei ao className:
+//    'text-left':
+//      Altera para 'text-align: left;'
+
+
+
+//  Perceba que por padrão o focus do meu notebook é branco, para alterar isso fiz o seguinte:
+//
+
+// Notebook - 10/02/2024 - 18h00 - 01:00:00 de vídeo
