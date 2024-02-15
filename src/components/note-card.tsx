134a8f57141e9076fdd7a7edf1b6bad597c663ee
Tierres Griep <tierresgriep@gmail.com>
2024-02-15 17:36:56 -0300

    PC - 15/02/2024 - 17h36 - Tierres G.

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -12,32 +12,4 @@ export function NoteCard() {
     )
 }
 
-//  Adicionei 'hover:border-2' ao className do card
-//  Ele altera a 'border-width' para 2px
-
-//  O problema é que essa borda ela ocupa espaço do elemento, então ao passar o mouse e ativar a bordar fica um efeito chato, onde o texto fica saindo do lugar
-
-//  Para resolver isso troquei o 'hover-border-2' por 'hover:ring-2' que aplica uma 'box shadow'
-//  Só que essa box shadow funciona da mesma forma que uma borda funciona, só que sem prejudicar o tamanho/posição das letras
-
-//  Para trocar a cor do 'ring-2' para a cor que está no figma adicionei logo em seguida 'hover:ring-slate-600'
-
-
-
-//  Alterei o note-card de 'div' para 'button', para eles ficarem clicáveis e termos estado de foco
-//  Acontece que por padrão os botões tem o texto centralizado, então adicionei ao className:
-//    'text-left':
-//      Altera para 'text-align: left;'
-
-
-
-//  Perceba que por padrão o focus do meu notebook é branco, para alterar isso fiz o seguinte:
-//  Utilizei 
-//  'focus-visible:ring-2 focus-visible:ring-lime-400'
-//  'outline-none'
-
-//    Utilizando 'focus' ao clicar em uma nota fica verde, pois ele aplica aquele CSS quando clicamos no elemento
-//    Já no 'focus-visible' ele aplica aquele CSS somente se estivermos navegando pelo teclado
-
-
-// Aula finalizada. PC - 15/02/2024 - 17h10 - último commit da aula 1
+// PC - 15/02/2024 - 17h33 - Tierres G. - Estudos Autodidatas.
--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
-
-// Sempre que falarmos 'renderizar' nada mais é do que 'mostra em tela'
-
-// No caso do código acima, ele pega a div com id 'root' e dentro dela ele cria a nossa aplicação, renderizando o App
-// " <React.StrictMode> " não é algo visível, ele apenas evita alguns errinhos, então é bom mantelo
