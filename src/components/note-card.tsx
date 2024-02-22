fbd89b7d77db0cc8a84237e638f212972830c5aa
Tierres Griep <tierresgriep@gmail.com>
2024-02-21 22:41:39 -0300

    PC - Começando a aula 2

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,11 +1,18 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps) {
     return (
-        <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+        <button className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
           <span className='text-sm font-medium text-slate-300'>
-            há 2 dias
+            {note.date.toISOString()}
           </span>
           <p className='text-sm leading-6 text-slate-400'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, iure dicta. Minima vel quo est impedit libero perspiciatis commodi omnis maiores quod accusantium. Nesciunt facilis fugit, sit reiciendis error nemo.
+            {note.content}
           </p>
           <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
         </button>
